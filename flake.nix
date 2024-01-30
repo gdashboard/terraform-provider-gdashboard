7df288f4a34c033e1d5d82ec56e6342cfcd25fdf
Maksym Ochenashko <maxochenashko@gmail.com>
2024-01-30 20:59:50 +0200

    Update nixpkgs to 23.11

--- flake.nix
+++ flake.nix
@@ -2,13 +2,9 @@
   description = "Grafana Dashboard Provider";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     hestia.url = "github:iRevive/hestia-nix";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
   outputs = { self, nixpkgs, flake-utils, hestia, ... }:
